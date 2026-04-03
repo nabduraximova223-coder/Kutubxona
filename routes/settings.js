d172f6db163b7500838f4cfd852cb2733489ce08
nabduraximova223-coder <nabduraximova223@gmail.com>
2026-04-03 13:12:04 +0500

    Upload: routes/settings.js

--- routes/settings.js
+++ routes/settings.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const db = require('../database');
 
 function isAuthenticated(req, res, next) {
